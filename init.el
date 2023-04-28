b6aa070537b29aacbeff3dc49071c8f03b231366
Вадим Берёзкин <berezkinve@codesrc.ru>
2023-04-28 18:14:02 +0300

    Customize cua-mode in welcome buffer.

--- init.el
+++ init.el
@@ -16,5 +16,19 @@
             (org-babel-load-file org-filename)
             (load-file el-filename))))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
 ;; Читаем первый литературный конфиг:
 (my/org-babel-load-file "literate-init")
